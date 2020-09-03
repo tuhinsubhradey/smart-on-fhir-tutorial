ecf77490648cade2113d6cb65af1c9a5cd6ae871
tuhinsubhradey <32764768+tuhinsubhradey@users.noreply.github.com>
2020-09-03 17:40:41 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -25,7 +25,9 @@
         var app = smart.patient.api.fetchAll({
                     type: 'Appointment',
                     query : {
-                      date : 'ge2018-12-07T22:22:16.270Z'
+                      date : 'ge2018-12-07T22:22:16.270Z',
+                      date : 'lt2020-08-14T22:22:16.270Z'
+                      
                     }
                   }); 
        
