e2f9daa8067e351aa0613217e2ea3e53ac443c9b
tuhinsubhradey <32764768+tuhinsubhradey@users.noreply.github.com>
2020-09-03 16:47:12 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -24,7 +24,9 @@
         
         var app = smart.patient.api.fetchAll({
                     type: 'Appointment',
-                    
+                    query : {
+                      date : '2020'
+                    }
                   }); 
        
         console.log("app ", app);
